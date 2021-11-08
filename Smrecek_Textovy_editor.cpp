e8d058a99d583ffaca0fd334c1c78b6ff54c3a14
Peter Smrecek <petersmrecek@gmail.com>
2021-11-08 18:59:10 +0100

    Add project files.

---
+++ Smrecek_Textovy_editor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+//int main(int argc, char* argv[])
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
